98d4cdc84072004d51f575cac060406bd4f377ae
Nil Ordoñez <nilordonez.7@gmail.com>
2018-04-25 14:01:06 +0200

    Cleaned up code

--- app/src/main/java/com/friendlyplaces/friendlyapp/activities/detailed_place/DetailedPlaceActivity.java
+++ app/src/main/java/com/friendlyplaces/friendlyapp/activities/detailed_place/DetailedPlaceActivity.java
@@ -95,7 +95,7 @@ public class DetailedPlaceActivity extends AppCompatActivity implements View.OnC
         getWindow().setEnterTransition(slideTransition);
         //getWindow().setSharedElementEnterTransition(TransitionInflater.from(this).inflateTransition(android.R.transition.move));
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
 
